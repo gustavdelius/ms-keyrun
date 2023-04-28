bfcdf96282439bff7f9000538fd1a046800c904f
Gustav Delius <gustav.delius@gmail.com>
2023-04-28 17:05:49 +0100

    Work in progress

--- mizer/understand_data.R
+++ mizer/understand_data.R
@@ -1,51 +1,121 @@
-remotes::install_github("NOAA-EDAB/ms-keyrun", ref = "sarah_wgsamsim")
+# Here I just explore the simulated data a bit
+
 library(mskeyrun)
 library(dplyr)
+library(ggplot2)
 
 # Species parameters ----
 
-# `focalSpecies` contains various species code for 11 species
-View(focalSpecies)
-unique(focalSpecies$LongName)
-# There are 16 rows because some species have two different codes in the
-# commercial fishing database NESPP3
-nrow(focalSpecies)
+species <- data.frame(
+    Name = c("Long_rough_dab",
+             "Green_halibut",
+             "Mackerel",
+             "Haddock",
+             "Saithe",
+             "Redfish",
+             "Blue_whiting",
+             "Norwegian_ssh",
+             "North_atl_cod",
+             "Polar_cod",
+             "Capelin"),
+    Long.Name = c("Long rough dab",
+                  "Greenland halibut",
+                  "Mackerel",
+                  "Haddock",
+                  "Saithe",
+                  "Redfish",
+                  "Blue whiting",
+                  "Norwegian spring spawning herring",
+                  "Northeast Atlantic Cod",
+                  "Polar cod",
+                  "Capelin"),
+    Latin = c("*Hippoglossoides platessoides*",
+              "*Reinhardtius hippoglossoides*",
+              "*Scomber scombrus*",
+              "*Melongrammus aeglefinus*",
+              "*Pollachius virens*",
+              "*Sebastes mentella*",
+              "*Micromesistius poutassou*",
+              "*Clupea harengus*",
+              "*Gadus morhua*",
+              "*Boreogadus saida*",
+              "*Mallotus villosus*"),
+    Code = c("LRD", "GRH", "MAC", "HAD", "SAI", "RED", 
+             "BWH", "SSH", "NCO", "PCO", "CAP")
+)
 
-# Let's use the long name as the species name in the mizer model but also
-# keep their modelName and the SVSPP code because that is used in other tables.
-sp = data.frame(species = unique(focalSpecies$LongName),
-                name = unique(focalSpecies$modelName),
-                SVSPP = unique(focalSpecies$SVSPP))
+head(simStartPars)
+unique(simStartPars$units)
+
+head(simBiolPar)
+
+# Survey data ----
+## Biomass ----
+
+head(simSurveyIndex)
+
+svbio <- simSurveyIndex |> filter(variable == "biomass")
+ggplot(svbio) +
+    geom_line(aes(x = year, y = value, color = survey)) +
+    theme(legend.position = "top") +
+    xlab("model year") +
+    ylab("tons") +
+    facet_wrap(~Name, scales = "free")
+
+## Length composition ----
+
+head(simSurveyLencomp)
+
+# convert strings to factors
+lencomp <- simSurveyLencomp |>
+    mutate(across(where(is.character), as.factor, .names = "{col}"))
+summary(lencomp)
+
+# We see that all observations have the same value for `ModSim`, `variable` and 
+# `units`. There are two surveys, one in the fall and one in the spring.
+
+## Age-size data ----
+
+### Mean weight at age ----
+
+head(simSurveyWtatAge)
+meanwatage <- simSurveyWtatAge |>
+    group_by(Name, age) |>
+    summarise(w = mean(value))
+
+ggplot(meanwatage) +
+    geom_line(aes(x = age, y = w)) +
+    facet_wrap(~Name, scales = "free")
+
+### Age-length composition
+
+head(simSurveyAgeLencomp)
+unique(simSurveyAgeLencomp$units)
+
+Lencomp <- simSurveyAgeLencomp |>
+    group_by(Name, survey, year, lenbin) |>
+    summarise(value = sum(value))
 
 
-# `realBiolPar` holds the length-weight parameters
-# The relation is W = aL^b e^Z where Z ~ IIDN(0,sigma^2)
-# so that E(W) = ae^((sigma^2)/2) L^b 
-realBiolPar = realBiolPar
-sp <- sp |>
-    left_join(realBiolPar, by = join_by(name == Name)) |>
-    mutate(species, name, SVSPP,
-           a = WLa * exp(sigma^2 / 2), 
-           b = WLb, WLsigma = sigma, .keep = "none")
+head(simSurveyAgecomp)
 
-# Survey abundance data ----
-# `surveyIndex`
-surveyIndex = surveyIndex
+meanw <- simSurveyAgeLencomp |>
+    left_join(simBiolPar, by = "Name") |>
+    # calculate mean of the weights at the length-class boundaries
+    mutate(w = WLa * (lenbin ^ WLb + (lenbin + 1) ^ WLb) / 2) |>
+    group_by(Name, agecl) |>
+    summarise(w = weighted.mean(w, value))
+    
+## Diet ----
 
-realSurveyLennumcomp = realSurveyLennumcomp
+head(simSurveyDietcomp)
 
-# `surveyIndexAll` contains the survey indices for all species
-# Could perhaps be used to determine what proportion of total biomass
-# comes from focal species
-surveyIndexAll = surveyIndexAll
+# Fishery data ----
 
-# `catchIndex` contains commercial landings and discards
-catchIndex = catchIndex
-unique(catchIndex$units)
+## Catch
 
-# Diet data ----
+head(simCatchIndex)
+unique(simCatchIndex$fishery)
 
-# `surveyDietcomp` surprisingly does not contain the size of the predator
-surveyDietcomp = surveyDietcomp
-
-simFocalSpecies = simFocalSpecies
+head(simFisheryLencomp)
+unique(simFisheryLencomp$units)
